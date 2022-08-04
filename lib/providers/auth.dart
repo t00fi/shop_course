1c72d6b5239a29394f7fac658090835083cde1c6
Mustafa <mcho.khalid@gmail.com>
2022-08-04 13:19:47 +0300

    adding autologin method to store user data tokens with shared pereference package

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
@@ -73,9 +72,43 @@ class Auth with ChangeNotifier {
       );
       _autoLogout();
       notifyListeners();
+      //storing auth data in memory by shared preference package.
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode(
+        {
+          'token': _token,
+          'userId': _userId,
+          'expiryDate': _expiryDateToken.toIso8601String(),
+        },
+      );
+      //create a key with the name userData and store userData variable values in it.
+      prefs.setString('userData', userData);
+      //call tryAutoLogin() method
     } catch (err) {
       //this catch hundling error is for other error not statuscode of http request
     }
+  }
+
+  //auto login mthod
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    //if we dont have data return false.
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedUserData = json.decode(prefs.getString('userData') as String)
+        as Map<String, dynamic>;
+    final expiryDate =
+        DateTime.parse(extractedUserData['expiryDate'] as String);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token = extractedUserData['token'] as String;
+    _userId = extractedUserData['userId'] as String;
+    _expiryDateToken = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
   }
 
   //signup method
@@ -91,13 +124,18 @@ class Auth with ChangeNotifier {
   }
 
   //logout by user when click on logout
-  void logout() {
+  Future<void> logout() async {
     _token = '';
     _userId = '';
     if (_authTimer.isActive) {
       _authTimer.cancel();
     }
     notifyListeners();
+    //clean shared preference
+    final prefs = await SharedPreferences.getInstance();
+    //since we only have user data to clear so we use clear() method to clear all references.
+    //if we had multiple instance we would use remove() method to only remove this reference user data.
+    prefs.clear();
   }
 
   void _autoLogout() {

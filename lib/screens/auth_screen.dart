f47d50c014fb237abdddf01a4520757de491992c
Mustafa <mcho.khalid@gmail.com>
2022-08-05 02:50:52 +0300

    adding animations

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -94,7 +94,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.login;
   final Map<String, String> _authData = {
@@ -103,6 +104,53 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  //controller for the animation
+  //i will nimate the white container curved in  300 millisconeds
+  late AnimationController _controller;
+  late Animation<Offset> _slideOffsetAnimation;
+  late Animation<double> _fadeOpacityAnimation;
+
+  @override
+  void initState() {
+    // TODO:set the value fo the controller and animation
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    /*
+    before AnimatedContainer
+    ********************************
+    // _heightAnimation = Tween<Size>(
+    //   begin: const Size(double.infinity, 260),
+    //   end: const Size(double.infinity, 320),
+    // ).animate(
+    //   //type of animation
+    //   CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+    // );
+    ***********************************
+    */
+
+    _fadeOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+    _slideOffsetAnimation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+    );
+
+    //instead of using listenrr we will use AnimationBuilder  in widget tree
+    //_heightAnimation.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -177,10 +225,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.signup;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.login;
       });
+      _controller.reverse();
     }
   }
 
@@ -192,12 +242,124 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      //will do same animation
+      child: AnimatedContainer(
+        duration: const Duration(microseconds: 300),
+        curve: Curves.fastOutSlowIn,
         height: _authMode == AuthMode.signup ? 320 : 260,
+        //after animation
+        //height: _heightAnimation.value.height,
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.signup ? 320 : 260),
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16.0),
+
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'E-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['email'] = value.toString();
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 5) {
+                      return 'Password is too short!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value.toString();
+                  },
+                ),
+                if (_authMode == AuthMode.signup)
+                  FadeTransition(
+                    opacity: _fadeOpacityAnimation,
+                    child: SlideTransition(
+                      position: _slideOffsetAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.signup,
+                        decoration: const InputDecoration(
+                            labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                const SizedBox(height: 20),
+                if (_isLoading)
+                  const CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    onPressed: _submit,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 8.0),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button!.color,
+                    child:
+                        Text(_authMode == AuthMode.login ? 'login' : 'SIGN UP'),
+                  ),
+                FlatButton(
+                  onPressed: _switchAuthMode,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  //shrink the tap area of the button
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text(
+                      '${_authMode == AuthMode.login ? 'SignUp' : 'Login'} INSTEAD'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*
+************************************
+was working with animated builder
+************************************
+the child property of Animated builder will not re-build.
+ch paramaeter is the value of the child property (the form which we have) which will not rebuild.
+**************************************
+ AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          //brfore animation
+          //height: _authMode == AuthMode.signup ? 320 : 260,
+          //after animation
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: const EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -276,6 +438,4 @@ class _AuthCardState extends State<AuthCard> {
           ),
         ),
       ),
-    );
-  }
-}
+*/
